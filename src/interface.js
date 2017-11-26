b7fbdf34192f90381544bb8392d3f9992a9321f6
Nero <neroshan12@gmail.com>
2017-11-26 16:53:08 +0000

    Add jQuery to full thermostat functionality

--- src/interface.js
+++ src/interface.js
@@ -1,15 +1,36 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
+  updateTemperature();
 
-  $('#temperature').text(thermostat.temperature);
+  $('#temperature-up').click(function() {
+    thermostat.up();
+    updateTemperature();
+  });
 
-  $('#temp-up').on('click', function() {
-    thermostat.increaseTemperature();
-  $('#temperature').text(thermostat.temperature);
+  $('#temperature-down').click(function() {
+    thermostat.down();
+    updateTemperature();
+  });
+
+  $('#temperature-reset').click(function() {
+    thermostat.resetTemperature();
+    updateTemperature();
+  });
+
+  $('#powersaving-on').click(function() {
+    thermostat.switchPowerSavingModeOn();
+    $('#power-saving').text('on')
+    updateTemperature();
   })
 
-  $('#temp-down').click(function() {
-    thermostat.decreaseTemperature();
-  $('#temperature').text(thermostat.temperature);
+  $('#powersaving-off').click(function() {
+    thermostat.switchPowerSavingModeOff();
+    $('#power-saving').text('off')
+    updateTemperature();
   })
-})
+
+  function updateTemperature() {
+    $('#temperature').text(thermostat.temperature);
+    $('#temperature').attr('class', thermostat.energyUsage());
+  };
+});
