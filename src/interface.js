cba5114e8d6f7df9dc4c90e6448b3e38e58b27cc
Nero <neroshan12@gmail.com>
2017-11-26 16:18:17 +0000

    Add jQuery for temp up and down

---
+++ src/interface.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  var thermostat = new Thermostat();
+
+  $('#temperature').text(thermostat.temperature);
+
+  $('#temp-up').on('click', function() {
+    thermostat.increaseTemperature();
+  $('#temperature').text(thermostat.temperature);
+  })
+
+  $('#temp-down').click(function() {
+    thermostat.decreaseTemperature();
+  $('#temperature').text(thermostat.temperature);
+  })
+})
